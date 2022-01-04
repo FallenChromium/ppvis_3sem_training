da1ae7bafc348bb553bb0b86f0beb36c4e523845
FallenChromium <fallenchromium@gmail.com>
2022-01-04 18:11:44 +0300

    Delete rootCatalogue variable; deleteFile method

--- AdminInterface.cpp
+++ AdminInterface.cpp
@@ -2,9 +2,6 @@
 
 using namespace cms;
 
-const Catalogue *rootCatalogue;
-std::unique_ptr<Catalogue> draftsCatalogue;
-
 
 void AdminInterface::createCatalogue(std::string name, std::shared_ptr<Catalogue> parent_catalogue) {
     parent_catalogue->insertCatalogue(std::make_unique<Catalogue>(Catalogue(name)));
@@ -14,4 +11,13 @@ void AdminInterface::moveFile(std::shared_ptr<Catalogue> old_catalogue, std::sha
     //insert first, because if in new_catalog there's another file with the same name, it'll throw
     new_catalogue->insertFile(file);
     old_catalogue->removeFile(file);
+}
+
+void AdminInterface::deleteFile(std::shared_ptr<File> file, std::shared_ptr<Catalogue> catalogue) {
+    //delete file in current directory
+    catalogue->removeFile(file);
+    //traverse catalogue system with dfs
+    for(auto subcatalogue : catalogue->getCatalogues() ) {
+        deleteFile(file, subcatalogue);
+    }
 }
